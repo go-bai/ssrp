f5357c57c879c558542f1b844d90538b58b97111
root <root@vmi729327.contaboserver.net>
2021-12-12 14:04:54 +0800

    add systemd

--- install.sh
+++ install.sh
@@ -14,11 +14,33 @@ echo "架构: ${arch}"
 
 last_version=$(curl -Ls "https://api.github.com/repos/go-bai/ssrp/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
 echo -e "检测到 ssrp 最新版本：${last_version}，开始安装"
+rm -rf /etc/ssrp
+mkdir /etc/ssrp/ -p
+cd /etc/ssrp
+
 wget https://github.com/go-bai/ssrp/releases/download/${last_version}/ssrp-${last_version}-linux-${arch}.tar.gz
 tar -xzf ssrp-${last_version}-linux-${arch}.tar.gz && rm -f ssrp-${last_version}-linux-${arch}.tar.gz
 chmod +x ssrp
-rm -f /usr/bin/ssrp && mv ssrp /usr/bin/ssrp
 
 wget https://raw.githubusercontent.com/go-bai/ssrp/master/config.toml
 
-echo -e "安装完成, 在配置文件路径下运行 ssrp 即可"
+cat > /etc/systemd/system/ssrp.service << EOF
+[Unit]
+Description=ssrp - simple small reverse proxy
+After=network.target
+[Service]
+User=$INSTALL_USER
+Type=simple
+Restart=always
+RestartSec=5s
+ExecStart=/etc/ssrp/ssrp
+WorkingDirectory=/etc/ssrp/
+LimitNPROC=10000
+LimitNOFILE=1000000
+[Install]
+WantedBy=multi-user.target
+EOF
+systemctl daemon-reload
+systemctl restart ssrp
+systemctl enable ssrp
+systemctl status ssrp
